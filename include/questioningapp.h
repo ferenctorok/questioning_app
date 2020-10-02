8ca53b7120d05b39c56ebc5bd050e213c30bf3f9
ferenctorok <ferike.trk@gmail.com>
2020-10-02 13:32:32 +0200

    CMakeLists.txt files overriden for new structure. constructor/destructor test.

--- include/questioningapp.h
+++ include/questioningapp.h
@@ -2,6 +2,11 @@
 #define QUESTIONINGAPP_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+
 
 class QuestioningApp : public QMainWindow
 {
@@ -10,5 +15,11 @@ class QuestioningApp : public QMainWindow
 public:
     QuestioningApp(QWidget *parent = nullptr);
     ~QuestioningApp();
+
+protected:
+    QWidget *centralwidget;
+    QPushButton *pushButton;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
 };
 #endif // QUESTIONINGAPP_H
--- src/questioningapp.cpp
+++ src/questioningapp.cpp
@@ -1,11 +1,31 @@
 #include "questioningapp.h"
+#include <QMenuBar>
+#include <QStatusBar>
 
 QuestioningApp::QuestioningApp(QWidget *parent)
     : QMainWindow(parent)
 {
+    centralwidget = new QWidget(this);
+    centralwidget->setObjectName(QStringLiteral("centralwidget"));
+    pushButton = new QPushButton(centralwidget);
+    pushButton->setObjectName(QStringLiteral("pushButton"));
+
+    this->setCentralWidget(centralwidget);
+    menubar = new QMenuBar(this);
+    menubar->setObjectName(QStringLiteral("menubar"));
+    menubar->setGeometry(QRect(0, 0, 800, 22));
+    this->setMenuBar(menubar);
+    statusbar = new QStatusBar(this);
+    statusbar->setObjectName(QStringLiteral("statusbar"));
+    this->setStatusBar(statusbar);
+
 }
 
 QuestioningApp::~QuestioningApp()
 {
+    delete centralwidget;
+    delete pushButton;
+    delete menubar;
+    delete statusbar;
 }
 
